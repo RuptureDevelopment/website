6d7ceb9cf3f071ac20689aed3dcb391fa417e93a
Juan Petterson Heberle <juan.petterson@gmail.com>
2019-03-05 00:05:21 -0300

    New website layout

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res ) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get('/development', (req, res ) => {
-    res.sendFile(path.join(__dirname + '/new-index.html'));
+app.get("/development", (req, res) => {
+  res.sendFile(path.join(__dirname + "/new-index.html"));
 });
 
-app.use(express.static(__dirname + '/Script'));
+app.get("/development2", (req, res) => {
+  res.sendFile(path.join(__dirname + "/new-index2.html"));
+});
+
+app.use(express.static(__dirname + "/Script"));
 //Store all JS and CSS in Scripts folder.
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
-app.use((req, res) =>{
-    if(req.secure){
-        next();
-    } else{
-        res.redirect( `https://${req.headers.host}${req.url}`);
-    }
+app.use((req, res) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
 });
 
-app.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
+app.listen(port, function() {
+  console.log("Umbler listening on port %s", port);
 });
